adf35ad89263249e2a7f17937edb411cf89a076c
Abhi Patel <abhi.d@simformsolutions.com>
2023-06-28 13:59:44 +0530

    add : test1 complete

--- Practical_11/Controllers/HomeController.cs
+++ Practical_11/Controllers/HomeController.cs
@@ -1,61 +1,103 @@
-﻿using Practical_11.Models;
-using System;
-using System.Collections.Generic;
+﻿using Practical_11.Data;
+using Practical_11.Models;
 using System.Web.Mvc;
 
 namespace Practical_11.Controllers
 {
 	public class HomeController : Controller
 	{
-		private static List<User> _users = new List<User>();
-
-		public HomeController()
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				_users.Add(new User()
-				{
-					Id = i,
-					Name = $"Test{i * 10}",
-					DOB = DateTime.Now.Date,
-					Address = $"hello this is address! {i * 11232}"
-				});
-			}
-		}
-
+		[HttpGet]
 		public ActionResult Index()
 		{
-			return View(_users);
+			return View(DataClass.Users());
 		}
 
+		[HttpGet]
 		public ActionResult Create()
 		{
 			return View(new User());
 		}
 
-		[HttpPost]
-		public ActionResult Create(User user)
+		[HttpGet]
+		public ActionResult Detail(int? id)
 		{
-			_users.Add(user);
-			return View(new User());
+			if (id == null)
+			{
+				return RedirectToAction("Error");
+			}
+			var user = DataClass.GiveMeUser(id ?? 0);
+			if (user == null)
+			{
+				return RedirectToAction("Error");
+			}
+			return View(user);
 		}
 
-
-		public ActionResult Update(int id)
+		public ActionResult Error()
 		{
 			return View();
 		}
 
-		public ActionResult Update(User user)
+		[HttpGet]
+		public ActionResult Update(int? id)
 		{
+			if (id == null)
+			{
+				return RedirectToAction("Error");
+			}
 
+			var user = DataClass.GiveMeUser(id ?? 0);
+
+			if (user == null)
+			{
+				return RedirectToAction("Error");
+			}
+
+			return View(user);
+		}
+
+		[HttpGet]
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return RedirectToAction("Error");
+			}
+
+			var user = DataClass.GiveMeUser(id ?? 0);
+
+			if (user == null)
+			{
+				return RedirectToAction("Error");
+			}
+			return View(user);
+		}
+
+		[HttpPost]
+		public ActionResult Delete(User user)
+		{
+			DataClass.DeleteUser(user.Id);
 			return RedirectToAction("Index");
 		}
 
-		public ActionResult Delete(int id)
+
+		[HttpPost]
+		public ActionResult Update(User user)
 		{
-			_users.Remove(_users.Find(x => x.Id == id));
-			return View();
+			if (user == null)
+			{
+				return RedirectToAction("Error");
+			}
+
+			DataClass.UpdateUser(user);
+			return RedirectToAction("Index");
+		}
+
+		[HttpPost]
+		public ActionResult Create(User user)
+		{
+			DataClass.AddUser(user);
+			return RedirectToAction("Index");
 		}
 
 	}
--- Practical_11/Models/User.cs
+++ Practical_11/Models/User.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Practical_11.Models
 {
 	public class User
 	{
+		[Key]
 		public int Id { get; set; }
+
+		[Required]
 		public string Name { get; set; }
 
-		public DateTime DOB { get; set; }
+		[Required]
+		[DataType(dataType: DataType.Date), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		public string DOB { get; set; }
+
 
 		public string Address { get; set; }
 
